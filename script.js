2dbf1b685d918d74c0a229a6983b7469534767d6
Murali4723 <114662863+Murali4723@users.noreply.github.com>
2024-09-15 10:14:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+function myred() {
+    document.getElementById('text').style.backgroundColor = "red";
+}
+function mygreen() {
+    document.getElementById('text').style.backgroundColor = 'green';
+}
+function myblue() {
+    document.getElementById('text').style.backgroundColor = 'blue';
+}
+
+function myviolet() {
+    document.getElementById('text').style.backgroundColor = 'violet';
+}
+function mypink() {
+    document.getElementById('text').style.backgroundColor = 'pink';
+}
+function myyellow() {
+    document.getElementById('text').style.backgroundColor = 'yellow';
+}
+function myorange() {
+    document.getElementById('text').style.backgroundColor = 'orange';
+}
+function myblack() {
+    document.getElementById('text').style.backgroundColor = 'black';
+}
+function myturquoise() {
+    document.getElementById('text').style.backgroundColor = 'turquoise';
+}
+function mypurple() {
+    document.getElementById('text').style.backgroundColor = 'purple';
+}
+function mybrown() {
+    document.getElementById('text').style.backgroundColor = 'brown';
+}
+function mygold() {
+    document.getElementById('text').style.backgroundColor = 'gold';
+}
+function mymaroon() {
+    document.getElementById('text').style.backgroundColor = 'maroon';
+}
+function mycadetblue() {
+    document.getElementById('text').style.backgroundColor = 'cadetblue';
+}
+function mygray() {
+    document.getElementById('text').style.backgroundColor = 'gray';
+}
+function myseagreen() {
+    document.getElementById('text').style.backgroundColor = 'seagreen';
+}
+function mydeeppink() {
+    document.getElementById('text').style.backgroundColor = 'deeppink';
+}
+function myindigo() {
+    document.getElementById('text').style.backgroundColor = 'indigo';
+}
+function mylightgreen() {
+    document.getElementById('text').style.backgroundColor = 'lightgreen';
+}
+function myyellowgreen() {
+    document.getElementById('text').style.backgroundColor = 'yellowgreen';
+}
+function mywhite() {
+    document.getElementById('text').style.backgroundColor = 'white';
+}
+function mydodgerblue() {
+    document.getElementById('text').style.backgroundColor = 'dodgerblue';
+}
+
+/*------*/
+
+
+
+function myfunredtext() {
+    document.getElementById('text').style.color = "red";
+}
+function myfungreentext() {
+    document.getElementById('text').style.Color = 'green';
+}
+function myfunbluetext() {
+    document.getElementById('text').style.color = 'blue';
+}
+
+function myfunviolettext() {
+    document.getElementById('text').style.color = 'violet';
+}
+function myfunpinktext() {
+    document.getElementById('text').style.color = 'pink';
+}
+function myfunyellowtext() {
+    document.getElementById('text').style.color = 'yellow';
+}
+function mysunorangetext() {
+    document.getElementById('text').style.color = 'orange';
+}
+function myfunblacktext() {
+    document.getElementById('text').style.color = 'black';
+}
+function myfunturquoisetext() {
+    document.getElementById('text').style.color = 'turquoise';
+}
+function myfunpurpletext() {
+    document.getElementById('text').style.color = 'purple';
+}
+function myfunbrowntext() {
+    document.getElementById('text').style.color = 'brown';
+}
+function myfungoldtext() {
+    document.getElementById('text').style.color = 'gold';
+}
+function myfunmaroontext() {
+    document.getElementById('text').style.color = 'maroon';
+}
+function myfuncadetbluetext() {
+    document.getElementById('text').style.color = 'cadetblue';
+}
+function myfungraytext() {
+    document.getElementById('text').style.color = 'gray';
+}
+function myfunseagreentext() {
+    document.getElementById('text').style.color = 'seagreen';
+}
+function myfundeeppinktext() {
+    document.getElementById('text').style.color = 'deeppink';
+}
+function myfunindigotext() {
+    document.getElementById('text').style.color = 'indigo';
+}
+function myfunlightgreentext() {
+    document.getElementById('text').style.color = 'lightgreen';
+}
+function myfunyellowgreentext() {
+    document.getElementById('text').style.color = 'yellowgreen';
+}
+function myfunwhitetext() {
+    document.getElementById('text').style.color = 'white';
+}
+function myfundodgerbluetext() {
+    document.getElementById('text').style.color= 'dodgerblue';
+}
